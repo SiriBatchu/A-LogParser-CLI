77133ab69781325a97d5fba95187eb04b1082d4d
SiriBatchu <bsirigupta@gmail.com>
2025-05-11 13:39:02 -0700

    Update Main.java

--- src/main/java/logparser/Main.java
+++ src/main/java/logparser/Main.java
@@ -1,1 +1,27 @@
+package logparser;
 
+public class Main {
+  public static void main(String[] args) {
+    String inputFile = null;
+    for (int i = 0; i < args.length; i++) {
+      if ("--file".equals(args[i]) && i + 1 < args.length) {
+        inputFile = args[i + 1];
+      }
+    }
+    if (inputFile == null) {
+      System.err.println("Usage: java Main --file <filename.txt>");
+      System.exit(1);
+    }
+
+    LogParser parser = new LogParser();
+    try {
+      parser.parseFile(inputFile);
+      JsonWriter.writeJson("apm.json", parser.getApmAggregator().getResult());
+      JsonWriter.writeJson("application.json", parser.getAppAggregator().getResult());
+      JsonWriter.writeJson("request.json", parser.getReqAggregator().getResult());
+      System.out.println("Aggregation complete. Output files: apm.json, application.json, request.json");
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
